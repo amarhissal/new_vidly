54528cc50b6be663dc122c6478e3e4a9f4bdebeb
amarhissal <108334744+amarhissal@users.noreply.github.com>
2023-12-22 10:49:11 +0530

    Update config.js
    changing from config to env file

--- startup/config.js
+++ startup/config.js
@@ -1,8 +1,8 @@
 
-const config = require('config');
+const env = require('dotenv').config();
 
 module.exports = function(){
-    if(!config.get('jwtPrivateKey')){
+    if(!process.env.jwtPrivateKey){
         console.error('FATAL ERROR : jwtPriavteKey is not defined');
         process.exit(1);
     }  
