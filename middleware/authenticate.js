d0adf4f18f47fff54443697b2dc84db86e704a0c
amarhissal <108334744+amarhissal@users.noreply.github.com>
2023-12-22 10:56:33 +0530

    Update authenticate.js
    changing from config to env

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,5 +1,5 @@
  const jwt = require('jsonwebtoken');
- const config = require('config');
+ const env = require('dotenv').config();
 
  function auth(req,res,next){
     const token = req.header('x-nm-token');
@@ -7,7 +7,7 @@
 if(!token) return res.status(401).send("Access Denied No Token Provided!");
 
 try{
-const decoded = jwt.verify(token,config.get('jwtPrivateKey'));
+const decoded = jwt.verify(token,process.env.jwtPrivateKey);
     req.user = decoded;
     next();
     }
